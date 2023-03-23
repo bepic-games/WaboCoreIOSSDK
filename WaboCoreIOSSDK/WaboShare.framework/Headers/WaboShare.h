f5adfc96b34f1fad5ce711047bdddbc12e870f3e
yilang <yilang@bepic.cc>
2023-03-23 10:53:07 +0800

    read to push

---
+++ WaboCoreIOSSDK/WaboShare.framework/Headers/WaboShare.h
@@ -0,0 +1,18 @@
+//
+//  WaboShare.h
+//  WaboShare
+//
+//  Created by Bepic on 2023/3/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaboShare.
+FOUNDATION_EXPORT double WaboShareVersionNumber;
+
+//! Project version string for WaboShare.
+FOUNDATION_EXPORT const unsigned char WaboShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaboShare/PublicHeader.h>
+
+
